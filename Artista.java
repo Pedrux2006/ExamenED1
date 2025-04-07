88a949600a7db0e501918a58257094cdafad97c0
Pedrux2006 <pedro.utrera.marin.alu@iesfernandoaguilar.es>
2025-04-07 10:05:11 +0200

    prueba1

---
+++ Artista.java
@@ -0,0 +1,25 @@
+/*Pedro Utrera Marin*/
+package evf.javadoc.original;
+
+public class Artista {
+    String nombreArtista;
+    public Artista(String nombreArtista) {
+        this.nombreArtista = nombreArtista;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "nombreArtista='" + nombreArtista + '\'' +
+                '}';
+    }
+
+    public String getNombreArtista() {
+        return nombreArtista;
+	//Prueba fetch
+	//Prueba fetch 2
+    }
+    
+}
+
+
